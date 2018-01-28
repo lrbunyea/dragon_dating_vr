301d7c051d8a95e7f13042e47e51d43b53662964
LEO-PC\lrbun <lrbunyea@wpi.edu>
2018-01-28 16:33:37 -0500

    Using fixed update instad of update? I'm  a dongus

--- DragonDatingVR/Assets/Scripts/BookStack/InstantiateBooks.cs
+++ DragonDatingVR/Assets/Scripts/BookStack/InstantiateBooks.cs
@@ -15,7 +15,7 @@ public class InstantiateBooks : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		if (count == 120)
         {
             count = 0;
